0fa6f3d6ff1f2dd53c4b8d06fd6454c647ae654f
CamilaCode7 <CamilaCode7>
2021-10-15 12:38:10 -0300

    adicionando controller

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const controller = require('./controllers/controller')
 const app = express();
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/products', require('./controllers/productController'));
+app.use('/products', controller);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
