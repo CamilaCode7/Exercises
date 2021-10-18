7d3a22bdcc363ebc6510618648b9837c56a7d002
CamilaCode7 <CamilaCode7>
2021-10-17 22:32:22 -0300

    update controller

--- controllers/Authors.js
+++ controllers/Authors.js
@@ -1,5 +1,6 @@
 const rescue = require('express-rescue');
 const service = require('../services/Author');
+const Joi = require('joi');
 
 const getAll = rescue(async (req, res) => {
    const author = await service.getAll();
@@ -7,6 +8,41 @@ const getAll = rescue(async (req, res) => {
 	 res.status(200).json(authors);
 });
 
+const findById = rescue(async (req, res, next) => {
+	const { id } = req.params;
+
+	const author = await service.findById(id);
+
+	if(author.error) return next(author.error);
+	res.status(200).json(author);
+})
+
+const create = rescue(async (req, res, next) => {
+  // Utilizo o Joi para descrever o objeto que espero
+  // receber na requisição. Para isso, chamo Joi.object()
+  // passando um objeto com os campos da requisição e suas descrições	
+	const { error } = Joi.object({
+		// Deve ser uma string (.string()) não vazia (.not().empty()) e é obrigatório (.required())
+		firstName: Joi.string().not().empty().required(),
+		// Não é obrigatório mas, caso seja informado, deve ser uma string não vazia
+		middleName: Joi.string().not().empty(),
+		lastName: Joi.string().not().empty().required(),
+	})
+	// Por fim, pedi que o Joi verifique se o corpo da requisição se adequa a essas regras
+	 .validate(req.body);
+	// Caso exista algum problema com a validação, iniciamos o fluxo de erro e interrompemos o middleware.
+  if (error) return next(error);
+	
+	const { firstName, middleName, lastName } = req.body;
+	const newAuthor = await service.create(firstName, middleName, lastName);
+	
+	if (newAuthor.error) return next(newAuthor.error);
+
+  return res.status(201).json(newAuthor);
+})
+
 module.exports = {
 	getAll,
+	findById,
+	create,
 }
