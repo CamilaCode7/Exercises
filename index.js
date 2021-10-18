6199797e61dfcac93bf1710d65e0f10f0c556a9e
CamilaCode7 <CamilaCode7>
2021-10-17 22:35:47 -0300

    adicionando controller

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const author = require('./controllers/Authors');
 const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/authors', author.getAll);
+app.get('/authors/:id', author.findById);
+app.post('/authors', author.create);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);  
