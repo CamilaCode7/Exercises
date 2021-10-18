4b8d56e660bb2b199c5dd23995227624b8305416
CamilaCode7 <CamilaCode7>
2021-10-17 22:42:20 -0300

    update middlewares de error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const author = require('./controllers/Authors');
+const error = require('./middlewares/error');
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,6 +10,9 @@ app.use(bodyParser.json());
 app.get('/authors', author.getAll);
 app.get('/authors/:id', author.findById);
 app.post('/authors', author.create);
+
+app.use(error);
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);  
