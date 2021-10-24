c30bdfa4c7397f72f811f469acdc01ec54558531
CamilaCode7 <CamilaCode7>
2021-10-24 16:59:37 -0300

    adicionando token

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,8 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+/* Chave secreta. É com ela que os dados do usuário serão encriptados. */
+   const secret = 'secretdetoken';
 
 module.exports = async (req, res) => {
   try {
@@ -12,7 +16,18 @@ module.exports = async (req, res) => {
   if (!user || user.password !== password)
     return res.status(401).json({ message: 'Usuário não existe ou senha inválida' });
 
-  return res.status(200).json({ message: 'Login efetuado com sucesso'});
+  // Crio uma config básica para o JWT, onde:
+  // expiresIn -> significa o tempo pelo qual esse token será válido;
+  // algorithm -> algoritmo que você usará para assinar a mensagem.
+
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: user }, secret, jwtConfig);
+
+  res.status(200).json({ token });
   } catch (err) {
     return res.status(500).json({ message: 'Erro interno', error: err.message });
   }
