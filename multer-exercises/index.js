f12b4f5e9dc5fd7b9bdfacbed5d1733d05af5c4f
CamilaCode7 <CamilaCode7>
2021-10-25 22:10:18 -0300

    create config multer

--- multer-exercises/index.js
+++ multer-exercises/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
 
 const { PORT } = process.env;
 
@@ -21,7 +23,41 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + '/uploads'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => { callback(null, 'uploads') },
+  filename: (req, file, callback) => { callback(null, `${Date.now()}-${file.originalname}`) }
+});
+
+// const upload = multer({ dest: 'uploads' });
+const upload = multer({ storage });
+
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype !== 'image/png') {
+    req.fileValidationError = true;
+
+    return cb(null, false);
+  }
+  if(fileExist(file.originalname)) {
+    req.fileDuplicated = true;
+
+    return cb(null, false)
+  }
+  cb(null, true);
+};
+
+const fileExist = (fileName) => {
+  //fs.readdirSync retorna uma lista com nome de todos os arquivos da pasta uploads.
+  const files = fs.readFileSync(`${__dirname}/uploads`);
+   //Aqui usamos a função some, que retorna `true` se algum dos items do array passar no teste, no nosso caso o `file.includes`.
+  return files.some(file => file === fileName);
+}
+
+
 app.get('/ping', controllers.ping);
+app.post('/upload', upload.single('file'), controllers.upload);
 
 app.use(middlewares.error);
 
