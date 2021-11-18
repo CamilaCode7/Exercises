f1eb81c731ff334df0eef45a1780e2a71a710696
CamilaCode7 <camilafabiabh9@gmail.com>
2021-11-18 11:14:16 -0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
+const randomHexColor = require('random-hex-color')();
+
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
+
+app.get('/', (_req, res) => {
+  res.type('text/html')
+  res.send(
+    `<body style="background-color: ${randomHexColor}">
+      <h1>A aplicação esta rodando no processo de id ${process.pid}!</h1>
+    </body>`
+  );
+});
 
 app.get('/hello', (_req, res) => {
-    res.send('Me first deploy-process-managers')  
+  res.send(`Meu id é ${process.pid}`)  
 });
 
 app.listen(port);
